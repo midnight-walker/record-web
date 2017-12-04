d62274a231664cdc5b3ea1b7e9b3d1dbab45060d
tangqiaojie <tangqiaojie@zbj.com>
2017-12-04 17:38:33 +0800

    dev

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import Login from './routes/Login';
 
 import Users from "./routes/Users.js";
 import Region from "./routes/Region.js";
@@ -8,15 +9,28 @@ import Station from "./routes/Station.js";
 import Defence from "./routes/Defence.js";
 import ScxcSupervisor from "./routes/ScxcSupervisor.js";
 
+function isLogin(nextState, replace){
+    if(window.location.search!=='?clkj'){
+        replace({
+            pathname: '/login',
+            state: {
+                nextPathname: nextState.location.pathname,
+                nextSearch: location.search
+            }
+        })
+    }
+}
+
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={IndexPage} />
-      <Route path="/users" component={Users} />
-      <Route path="/region" component={Region} />
-      <Route path="/station" component={Station} />
-      <Route path="/defence" component={Defence} />
-      <Route path="/scxcSupervisor" component={ScxcSupervisor} />
+      <Route path="/login" component={Login} />
+      <Route path="/" onEnter={isLogin} component={IndexPage} />
+      <Route path="/users" onEnter={isLogin} component={Users} />
+      <Route path="/region" onEnter={isLogin} component={Region} />
+      <Route path="/station" onEnter={isLogin} component={Station} />
+      <Route path="/defence" onEnter={isLogin} component={Defence} />
+      <Route path="/scxcSupervisor" onEnter={isLogin} component={ScxcSupervisor} />
     </Router>
   );
 }
---
+++ src/routes/login.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tqj <2482366539@qq.com> on 2017/12/4.
+ */
+import React from 'react';
+import { connect } from 'dva';
+
+function IndexPage({ location }) {
+    return (
+            <form action="/doLogin">
+                <input name="input"/>
+                <input name="password"/>
+                <button type="submit">登录</button>
+            </form>
+    );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
